9cbd3f1bb19d188c22446b48e98860dc1651c71e
SponczBoncz <bku82@o2.pl>
2024-03-24 07:25:18 +0100

    FoG alpha version

--- project/gamelist/forms.py
+++ project/gamelist/forms.py
@@ -2,10 +2,29 @@ from django import forms
 
 from django.core.validators import URLValidator
 
-from .models import Game, Category, GameMechanic
+from .models import Game, Category, GameMechanic, RequestedCategory, RequestedMechanic, STATUS_CHOICES
 
 
 class AddGameForm(forms.ModelForm):
+    '''
+    Description:
+        Form for adding a new game to the system.
+
+    Fields:
+        title: CharField for the game title.
+        author: CharField for the game author.
+        description: TextField for the game description.
+        min_players: IntegerField for the minimum number of players.
+        max_players: IntegerField for the maximum number of players.
+        game_time: CharField for specifying the game time.
+        category: CheckboxSelectMultiple for selecting game categories.
+        game_mechanics: CheckboxSelectMultiple for selecting game mechanics.
+        bgg_link: URLInput for specifying a BoardGameGeek link.
+
+    Customization:
+        Sets widget attributes for input validation.
+    '''
+
     class Meta:
         model = Game
         fields = [
@@ -30,12 +49,22 @@ class AddGameForm(forms.ModelForm):
         self.fields['max_players'].widget.attrs['min'] = 1
         self.fields['game_time'].widget.attrs['maxlength'] = 64
         self.fields['category'].widget = forms.CheckboxSelectMultiple()
-        self.fields['category'].queryset = Category.objects.all()
+        self.fields['category'].queryset = Category.objects.order_by('name')
         self.fields['game_mechanics'].widget = forms.CheckboxSelectMultiple()
-        self.fields['game_mechanics'].queryset = GameMechanic.objects.all()
+        self.fields['game_mechanics'].queryset = GameMechanic.objects.order_by('name')
         self.fields['bgg_link'].widget = forms.URLInput()
 
 class EditGameForm(forms.ModelForm):
+    '''
+    Description:
+        Form for editing an existing game.
+
+    Fields:
+        Same as AddGameForm.
+
+    Customization:
+        Sets initial values for fields based on the current values in the model instance.
+    '''
 
     class Meta:
         model = Game
@@ -65,7 +94,251 @@ class EditGameForm(forms.ModelForm):
         self.fields['max_players'].widget.attrs['min'] = 1
         self.fields['game_time'].widget.attrs['maxlength'] = 64
         self.fields['category'].widget = forms.CheckboxSelectMultiple()
-        self.fields['category'].queryset = Category.objects.all()
+        self.fields['category'].queryset = Category.objects.order_by('name')
         self.fields['game_mechanics'].widget = forms.CheckboxSelectMultiple()
-        self.fields['game_mechanics'].queryset = GameMechanic.objects.all()
+        self.fields['game_mechanics'].queryset = GameMechanic.objects.order_by('name')
         self.fields['bgg_link'].widget = forms.URLInput()
+
+class AddMechanicForm(forms.ModelForm):
+    '''
+    Description:
+        Form for adding a new game mechanic.
+
+    Fields:
+        name: CharField for the mechanic name.
+        description: TextField for the mechanic description.
+        status: ChoiceField for selecting the status of the mechanic.
+
+    Customization:
+        Sets widget attributes for input validation.
+    '''
+
+    class Meta:
+        model = RequestedMechanic
+        fields = [
+            'name',
+            'description',
+            'status'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')  # Get the instance from kwargs
+        super(AddMechanicForm, self).__init__(*args, **kwargs)
+
+        if instance:
+            # Set the initial value for each field based on the current value in the model instance
+            for field_name in self.fields:
+                self.fields[field_name].widget.attrs['value'] = getattr(instance, field_name)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+        status = forms.ChoiceField(choices=STATUS_CHOICES)
+
+
+class AddCategoryForm(forms.ModelForm):
+    '''
+    Description:
+        Form for adding a new game category.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = RequestedCategory
+        fields = [
+            'name',
+            'description',
+            'status'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')  # Get the instance from kwargs
+        super(AddCategoryForm, self).__init__(*args, **kwargs)
+
+        if instance:
+            # Set the initial value for each field based on the current value in the model instance
+            for field_name in self.fields:
+                self.fields[field_name].widget.attrs['value'] = getattr(instance, field_name)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+        status = forms.ChoiceField(choices=STATUS_CHOICES)
+
+
+class AddNewCategoryForm(forms.ModelForm):
+    '''
+    Description:
+        Form for adding a new category.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = Category
+        fields = [
+            'name',
+            'description'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(AddNewCategoryForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+
+
+class AddNewMechanicForm(forms.ModelForm):
+    '''
+    Description:
+        Form for adding a new game mechanic.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = GameMechanic
+        fields = [
+            'name',
+            'description'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(AddNewMechanicForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+
+
+class AskForCategoryForm(forms.ModelForm):
+    '''
+    Description:
+        Form for requesting a new game category.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = RequestedCategory
+        fields = [
+            'name',
+            'description'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(AskForCategoryForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+
+class AskForMechanicForm(forms.ModelForm):
+    '''
+    Description:
+        Form for requesting a new game mechanic.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = RequestedMechanic
+        fields = [
+            'name',
+            'description'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(AskForMechanicForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+
+
+class EditCategoryForm(forms.ModelForm):
+    '''
+    Description:
+        Form for editing an existing game category.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = Category
+        fields = ['name',
+                  'description',
+                  ]
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')  # Get the instance from kwargs
+        super(EditCategoryForm, self).__init__(*args, **kwargs)
+
+        # Set the initial value for each field based on the current value in the model instance
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['value'] = getattr(instance, field_name)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+
+
+class EditMechanicForm(forms.ModelForm):
+    '''
+    Description:
+        Form for editing an existing game mechanic.
+
+    Fields:
+        Same as AddMechanicForm.
+
+    Customization:
+        Same as AddMechanicForm.
+    '''
+
+    class Meta:
+        model = GameMechanic
+        fields = ['name',
+                  'description',
+                  ]
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')  # Get the instance from kwargs
+        super(EditMechanicForm, self).__init__(*args, **kwargs)
+
+        # Set the initial value for each field based on the current value in the model instance
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['value'] = getattr(instance, field_name)
+
+        self.fields['name'].widget.attrs['maxlength'] = 255
+        self.fields['description'].widget = forms.Textarea()
+
+
+class SearchGameForm(forms.Form):
+    '''
+    Description:
+        Form for searching games based on title.
+
+    Fields:
+        title: CharField for entering the game title.
+    '''
+
+    title = forms.CharField(max_length=255)
--- project/userbase/forms.py
+++ project/userbase/forms.py
@@ -1,52 +1,176 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from .models import User
+from .models import User, GameInvitation, Game
 
 class LoginForm(forms.Form):
-    login = forms.CharField(widget=forms.TextInput)
+    """
+    Description:
+        Form for user login.
+
+    Fields:
+        email: CharField for user email.
+        password: CharField for user password.
+
+    Methods:
+        clean: Custom validation method to authenticate the user.
+    """
+
+    email = forms.CharField(widget=forms.EmailInput)
     password = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self):
         cleaned_data = super().clean()
-        username = cleaned_data.get('login')
+        username = cleaned_data.get('email')
         password = cleaned_data.get('password')
         self.user = authenticate(username=username, password=password)
         if self.user is None:
-            raise ValidationError("Nieprawidłowe hasło lub login")
+            raise ValidationError("Nieprawidłowe hasło lub email")
 
 
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label='Hasło')
-    password2 = forms.CharField(widget=forms.PasswordInput, max_length=64, label='Potwierdź hasło')
+
+class UserCreationForm(UserCreationForm):
+    '''
+    Description:
+        Form for user registration.
+
+    Fields:
+        nick: CharField for user nickname.
+        email: CharField for user email.
+        password1: CharField for user password.
+        password2: CharField for password confirmation.
+    '''
+
     class Meta:
         model = User
+        fields = ['nick', 'email', 'password1', 'password2']  # Include email, password1, and password2 fields
+        # fields = ("email",)
+
+
+class UserChangeForm(UserChangeForm):
+    '''
+    Description:
+        Form for user profile editing.
+
+    Fields:
+        nick: CharField for user nickname.
+        email: CharField for user email.
+        password: CharField for user password.
+    '''
+
+    class Meta:
+        model = User
+        fields = ['nick', 'email', 'password']  # Include email, password1, and password2 fields
+
+
+
+
+class AddInvitationForm(forms.ModelForm):
+    '''
+    Description:
+        Form for adding a game invitation.
+
+    Fields:
+        game: Select field for selecting a game from the user's connected games.
+        no_players: Integer field for specifying the number of players.
+        game_time: CharField for specifying the game time.
+        game_place: CharField for specifying the game place.
+        extra_text: CharField for additional text.
+
+    Customization:
+        Filters games based on the current user's connected games.
+        Sets widget attributes for input validation.
+    '''
+
+    class Meta:
+        model = GameInvitation
         fields = [
-            'login',
-            'email',
-            'password',
+            'game',
+            'no_players',
+            'game_time',
+            'game_place',
+            'extra_text'
         ]
 
-    def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)
 
-        self.fields['login'].widget.attrs['maxlength'] = 64
-        self.fields['email'].widget = forms.EmailInput()
-        self.fields['password'].widget.attrs['maxlength'] = 64
+    def __init__(self, user, *args, **kwargs):
+        super(AddInvitationForm, self).__init__(*args, **kwargs)
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        password2 = cleaned_data.get('password2')
 
-        if password and password2 and password != password2:
-            raise ValidationError("Hasła nie są takie same!")
+        # Set the widget for the 'game' field to CheckboxSelectMultiple
+        self.fields['game'].widget = forms.Select()
+        # Filter games based on the current user's connected games
+        self.fields['game'].queryset = user.games.all()
+        self.fields['no_players'].widget.attrs['min'] = 1
+        self.fields['game_time'].widget.attrs['maxlength'] = 255
+        self.fields['game_place'].widget.attrs['maxlength'] = 255
+        self.fields['extra_text'].widget.attrs['maxlength'] = 510
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        # user.set_password(self.cleaned_data['password'])
-        if commit:
-            user.save()
-        return user
+
+class EditInvitationForm(forms.ModelForm):
+    '''
+    Description:
+        Form for editing a game invitation.
+
+    Fields:
+        game: Select field for selecting a game from the user's connected games.
+        no_players: Integer field for specifying the number of players.
+        game_time: CharField for specifying the game time.
+        game_place: CharField for specifying the game place.
+        extra_text: CharField for additional text.
+
+    Customization:
+        Filters games based on the current user's connected games.
+        Sets widget attributes for input validation.
+    '''
+
+    class Meta:
+        model = GameInvitation
+        fields = [
+            'game',
+            'no_players',
+            'game_time',
+            'game_place',
+            'extra_text'
+        ]
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditInvitationForm, self).__init__(*args, **kwargs)
+
+        # Set the widget for the 'game' field to Select
+        self.fields['game'].widget = forms.Select()
+
+        # Filter games based on the current user's connected games
+        self.fields['game'].queryset = user.games.all()
+
+        self.fields['no_players'].widget.attrs['min'] = 1
+        self.fields['game_time'].widget.attrs['maxlength'] = 255
+        self.fields['game_place'].widget.attrs['maxlength'] = 255
+        self.fields['extra_text'].widget.attrs['maxlength'] = 510
+
+
+class LuckyShotForm(forms.ModelForm):
+    '''
+    Description:
+        Form for selecting the number of players for a lucky shot game.
+
+    Fields:
+        no_players: Integer field for specifying the number of players.
+
+    Customization:
+        Sets a custom validation rule for the number of players.
+
+    '''
+
+    # Custom field for number of players
+    no_players = forms.IntegerField(min_value=1)
+
+    class Meta:
+        model = Game
+        fields = []
+
+
+
 
